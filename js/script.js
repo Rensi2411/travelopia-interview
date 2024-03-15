c2b7c1bbe31871a1dd5e9cdc82e134af6c075dcd
Rensi <rensidudhat2411@gmail.com>
2024-03-15 14:44:05 +0530

    add comments

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
+// Function to show a simple alert with "Hello, World!"
 function showAlert() {
     alert("Hello, World!");
 }
 
+// Function to toggle the visibility of the navigation menu
 function toggleMenu() {
     var nav = document.querySelector('nav');
     nav.classList.toggle('show-menu');
 }
 
+// Function to show a styled alert container
 function showStyledAlert() {
     document.getElementById('styledAlert').style.display = "block";
 }
 
+// Function to close the styled alert container
 function closeStyledAlert() {
     document.getElementById('styledAlert').style.display = "none";
 }
